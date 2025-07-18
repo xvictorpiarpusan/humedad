d3eaf757a77a9751e8d7bff383cbaf26a31b161d
Gnowak99 <spartanbets98@gmail.com>
2025-07-18 08:01:45 -0500

    Conexion base de datos

--- sensor.py
+++ sensor.py
@@ -2,6 +2,7 @@ from grovepi import *
 from grove_rgb_lcd import *
 from time import sleep
 from math import isnan
+import MySQLdb
 
 # Configuración del sensor DHT
 dht_sensor_port = 7
@@ -9,6 +10,20 @@ dht_sensor_type = 0  # 0 = DHT11 (azul), 1 = DHT22 (blanco)
 
 # Color del fondo del LCD
 setRGB(0, 255, 0)
+
+# Conectar a la base de datos MariaDB
+try:
+    conn = MySQLdb.connect(
+        host="localhost",
+        user="prueba_user1",
+
+        passwd="1234",
+        db="SensorHome"
+    )
+    cursor = conn.cursor()
+except Exception as e:
+    print("Error al conectar a la base de datos:", e)
+    exit(1)
 
 # Bucle principal
 while True:
@@ -23,8 +38,16 @@ while True:
         # Mostrar en pantalla LCD
         setText_norefresh("Temp:" + str(temp) + "C\n" + "Humidity :" + str(hum) + "%")
 
-    except (IOError, TypeError) as e:
+        # Insertar en la base de datos
+        query = "INSERT INTO Clima (temperatura, humedad) VALUES (%s, %s)"
+        cursor.execute(query, (temp, hum))
+        conn.commit()
+
+    except:
         pass
 
     sleep(2)
 
+# Cerrar conexión
+cursor.close()
+conn.close()
